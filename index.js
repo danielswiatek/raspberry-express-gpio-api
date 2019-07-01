25eb8ac71d34debc58035e2d1ee9620d1e4a8e92
Daniel Swiatek <daniel.swiatek@codeanker.de>
2019-07-01 16:27:05 +0200

    edit

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const Gpio = require('onoff').Gpio;
 
 app.get('/tueroeffnen', function (req, res) {
     const led = new Gpio(5, 'out');       // Export GPIO17 as an output
+    let stopBlinking = false;
 
-    console.log(led.write(1));
-    
+    // Toggle the state of the LED connected to GPIO17 every 200ms
+    const blinkLed = () => {
+        if (stopBlinking) {
+        return led.unexport();
+        }
+
+        led.read()
+        .then(value => led.write(value ^ 1))
+        .then(_ => setTimeout(blinkLed, 200))
+        .catch(err => console.log(err));
+    };
+
+    blinkLed();
+
+    // Stop blinking the LED after 5 seconds
+    setTimeout(_ => stopBlinking = true, 5000);
     res.status(200).send('Hello World!');
 });
 
